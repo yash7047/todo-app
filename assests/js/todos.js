5f290cf9051ade7a243af901cf44560f4c27eeb5
yash7047 <yashjaiswal895@gmail.com>
2020-09-02 11:12:35 +0530

    todo project

---
+++ assests/js/todos.js
@@ -0,0 +1,17 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+	$(this).parent().remove();
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var todoText=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+todoText+"</li>"); 
+	}
+});
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
